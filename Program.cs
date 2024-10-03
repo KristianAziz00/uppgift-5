172a1ea9528dbaa1bb3eb8a97ebb2b79e263297d
Kristian <Kristian@LAPTOP-K75FUMAK>
2024-10-03 10:10:26 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿Console.WriteLine("Kalkylator");
+Console.WriteLine("Välj en operation (+, -, , /):");
+char operation = Console.ReadKey().KeyChar;
+
+Console.WriteLine("\nAnge första talet:");
+double num1 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Ange andra talet:");
+double num2 = Convert.ToDouble(Console.ReadLine());
+
+double result;
+
+switch (operation)
+{
+    case '+':
+        result = num1 + num2;
+        break;
+    case '-':
+        result = num1 - num2;
+        break;
+    case '*':
+        result = num1 * num2;
+        break;
+    case '/':
+        result = num1 / num2;
+        break;
+    default:
+        Console.WriteLine("Fel: Ogiltig operation.");
+        return;
+}
+
+Console.WriteLine($"Resultat: {result}");
+Console.ReadKey();
